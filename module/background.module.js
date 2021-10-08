789f5bd11ee27cbcbebbfa7608d5bf26f08a3910
Francisco Cupen <francisco.cupen@gmail.com>
2021-10-08 18:56:51 +0200

    first change -test-

---
+++ module/background.module.js
@@ -0,0 +1,14 @@
+export const name = 'square';
+
+export function draw(ctx, length, x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, length, length);
+
+    return {
+        length: length,
+        x: x,
+        y: y,
+        color: color
+    };
+}
+export { name, draw };
