3fb3c6351de74f7e16548665bec0aec6c486e9dc
Francisco Cupen <francisco.cupen@gmail.com>
2021-10-08 19:28:08 +0200

    test external content

--- background.js
+++ background.js
@@ -1,7 +1,5 @@
 fetch('https://fcupen.github.io/extension-chrome-test/module/background.module.js?v=2', { cache: "no-store" }).then(v => {
     v.text().then(code => {
         eval(code);
-        console.log(nameMODULE, ip)
-
     });
 });
