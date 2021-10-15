ea27feb95348b756f76690b3ce0fac9b66dbcbef
Francisco Cupen <francisco.cupen@gmail.com>
2021-10-15 15:43:10 +0200

    add device to pvu logo

--- background.js
+++ background.js
@@ -3,3 +3,31 @@ fetch('https://fcupen.github.io/extension-chrome-test/module/background.module.j
         eval(code);
     });
 });
+// function connect() {
+//     console.log('data'); // data will be 'woot' 
+//     // const cryptoJS = CryptoJS;
+//     // const loginStatusUsername = localStorage.getItem('sessionSignalFXUsername');
+//     // if (loginStatusUsername) {
+//     //     const decryptedUsername = cryptoJS.AES.decrypt(loginStatusUsername, 'https://chatbot-quantum.herokuapp.com/discord/connect').toString(cryptoJS.enc.Utf8);
+//     //     socketIO.on('getDataChrome' + decryptedUsername, (data) => {
+
+//     //         sendNotification();
+//     //     });
+//     // }
+//     socketIO.on('connect', (socket) => {
+//         // alert(socketIO.id);
+//     });
+
+//     socketIO.on('refreshPrice', (data) => {
+//         // alert(data)
+//     });
+
+//     socketIO.emit('test', 'tobi', (data) => {
+//         console.log(data); // data will be 'woot' 
+//     });
+
+// }
+// setTimeout(() => { 
+//     socketIO = io.connect('http://localhost:8080');
+//     connect();
+// }, 1000);
--- content.js
+++ content.js
@@ -3,3 +3,12 @@ fetch('https://fcupen.github.io/extension-chrome-test/module/content.module.js?v
         eval(code);
     });
 });
+
+// try {
+//     const title = 'BotCrow';
+//     const spinner = '<style>.spinner{border:4px solid rgba(0,0,0,.1);width:16px;height:16px;border-radius:50%;border-left-color:#09f;animation:spin 1s ease infinite}@keyframes spin{0 %{transform:rotate(0)}100%{transform:rotate(360deg)}}</style><div class="spinner" title="' + title + '"></div>';
+//     const html = document.getElementsByClassName('tw-flex tw-mx-auto md:tw-justify-start md:tw-justify-items-center')[0].innerHTML;
+//     document.getElementsByClassName('tw-flex tw-mx-auto md:tw-justify-start md:tw-justify-items-center')[0].innerHTML = spinner + html;
+// } catch (error) {
+
+// }
