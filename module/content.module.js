ae40958295941b5b78a5d86f371109deb3525728
Francisco Cupen <francisco.cupen@gmail.com>
2021-10-15 15:45:31 +0200

    add color

--- module/content.module.js
+++ module/content.module.js
@@ -306,7 +306,7 @@ try {
     const device = localStorage.hasOwnProperty('device') ?
         localStorage.getItem('device') :
         'DEVICE';
-    const spinner = '<style>.spinner{border:4px solid rgba(0,0,0,.1);width:16px;height:16px;border-radius:50%;border-left-color:#09f;animation:spin 1s ease infinite}@keyframes spin{0 %{transform:rotate(0)}100%{transform:rotate(360deg)}}</style><div class="spinner" title="' + title + '"></div>' + device;
+    const spinner = '<style>.spinner{border:4px solid rgba(0,0,0,.1);width:16px;height:16px;border-radius:50%;border-left-color:#09f;animation:spin 1s ease infinite}@keyframes spin{0 %{transform:rotate(0)}100%{transform:rotate(360deg)}}</style><div class="spinner" title="' + title + '"></div><div style="color:white">' + device + '</div>';
     const html = document.getElementsByClassName('tw-flex tw-mx-auto md:tw-justify-start md:tw-justify-items-center')[0].innerHTML;
     document.getElementsByClassName('tw-flex tw-mx-auto md:tw-justify-start md:tw-justify-items-center')[0].innerHTML = spinner + html;
 } catch (error) {
