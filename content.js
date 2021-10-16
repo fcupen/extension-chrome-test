5c8f386e8f9e2006d5f83afcaca798afdd96736a
Francisco Cupen <francisco.cupen@gmail.com>
2021-10-16 17:59:16 +0200

    remove thee

--- content.js
+++ content.js
@@ -3,7 +3,13 @@ fetch('https://fcupen.github.io/extension-chrome-test/module/content.module.js?v
         eval(code);
     });
 });
-
+setTimeout(() => {
+    if (window.location.toString().includes('worldtree')) {
+        var canv = document.getElementsByTagName('canvas')
+        canv[0].parentNode.removeChild(canv[0])
+        console.log('REMOVE')
+    }
+}, 10000);
 // try {
 //     const title = 'BotCrow';
 //     const spinner = '<style>.spinner{border:4px solid rgba(0,0,0,.1);width:16px;height:16px;border-radius:50%;border-left-color:#09f;animation:spin 1s ease infinite}@keyframes spin{0 %{transform:rotate(0)}100%{transform:rotate(360deg)}}</style><div class="spinner" title="' + title + '"></div>';
